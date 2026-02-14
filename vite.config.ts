68d8ffb4874d9bfa9e4e3531463dffb4aec9fb15
Christopher Trigg <chris.trigg@gmail.com>
2026-02-14 13:54:25 +1300

    rename chart to plotter

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: 'index.html',
-        chart: 'plotter.html',
+        plotter: 'plotter.html',
 		history: 'history.html'
       }
     }
